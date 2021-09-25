9fde69beba6855d2c0b13b8d9df5157cb89bb3fc
programmertaha <tahasaad@unitedsystems-iq.com>
2021-09-25 12:29:32 +0300

    completing the tasks

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -17,6 +17,15 @@ class View {
   }
 }
 
+class Text extends View {
+  String content;
+  Text({
+    required int id,
+    required this.content,
+    Color? color,
+  }) : super(id, color: color);
+}
+
 void main() {
   /*
     1) Create class named `Text` that extends/inherits `View` class,
@@ -26,18 +35,29 @@ void main() {
     2) Create new `Text` object with the following:
       var helloText = Text(<random id>, content: 'Hello' )
   */
-
   int id = Random().nextInt(10000);
+  var helloText = Text(
+    id: id,
+    content: 'Hello',
+    color: Color(0xff0000),
+  );
 
   print('hello: $helloText');
+  print('$helloText');
+  task2();
 }
 
 void task2() {
   List<int> numbers = List.generate(75, (index) => Random().nextInt(10000));
+  List<int> evenNumbers = [];
+  print(numbers);
+  numbers.forEach((num) {
+    if (num % 2 == 0) evenNumbers.add(num);
+  });
 
   /*
     Separate even numbers from the above `numbers` list.
     List<int> evenNumbers = ...
   */
-  print('evenNumbers: $evenNumbers');
+  print('evenNumbers2: $evenNumbers');
 }
